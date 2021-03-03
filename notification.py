bb4205fd7935975cb678e90428d69fb7e534f483
huangg233 <74952471+huangg233@users.noreply.github.com>
2021-03-03 06:40:16 +0000

    Update notification.py

--- notification.py
+++ notification.py
@@ -10,9 +10,9 @@ import time
 #######################
 
 # [0,1,2,3]  0:不通知     1:server酱      2:SMTP邮件服务        3:bark服务
-needYou2Know = 0
+needYou2Know = 1
 
-SCKEY = ''        # Server酱的SCKEY
+SCKEY = 'SCU138344T2c331cde6553c899e669cfb39d4383425fe31173b692e'        # Server酱的SCKEY
 
 email_dict = {
     "sender": '',                 # ① sender是邮件发送人邮箱
